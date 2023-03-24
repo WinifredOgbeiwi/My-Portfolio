2a09afe555e13bc1f30546b3174903bd60387eb6
winifredogbeiwi <imadeogbeiwi@gmail.com>
2023-03-24 16:14:05 +0100

    margin

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -12,11 +12,11 @@ function Hero() {
           <h1 
               data-aos='fade-left'
           className='text-4xl sm:text-7xl '>Winifred ogbeiwi</h1>
-            <p data-aos='fade-right' className='mt-5 text-xs sm:text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, labore. Lorem ipsum dolor, <br/>sit amet consectetur adipisicing elit. Iure, sequi?</p>
+            <p data-aos='fade-right' className='mt-10 text-xs sm:text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, labore. Lorem ipsum dolor, <br/>sit amet consectetur adipisicing elit. Iure, sequi?</p>
 
             <div data-aos='fade-left'>
               <a href="./Winifred_Ogbeiwi_CV.pdf" download={true}>
-                <button className=' w-fit px-6 py-3 my-2 flex items-center rounded-md bg-white cursor-pointer group shadow-2xl gap-4 text-lg'>Download CV
+                <button className=' w-fit px-6 py-3 my-2 flex items-center rounded-md bg-white cursor-pointer group shadow-2xl gap-4 text-lg mt-10'>Download CV
 
                   <span className='group-hover:rotate-90 duration-300'><MdDoubleArrow />
                   </span>
