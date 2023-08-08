aec8fd4c83ba03dc8da6fa3201c0e6bc77df4d95
winifredogbeiwi <imadeogbeiwi@gmail.com>
2023-08-08 17:20:07 +0100

    addidtion of chat box

--- src/components/socialMedias/SocialMedia.jsx
+++ src/components/socialMedias/SocialMedia.jsx
@@ -8,7 +8,6 @@ function SocialMedia() {
         {SmediaData.map(({ id, child, href }) => (
           <li key={id}
             className={'flex justify-between items-center w-40 h-10 bg-transparent px-5 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-1000'}
-
           >
             <a href={href} className='flex justify-between items-center w-full text-black bg-transparent'
               target='_blank'
@@ -24,4 +23,4 @@ function SocialMedia() {
   )
 }
 
-export default SocialMedia
+export default SocialMedia;
